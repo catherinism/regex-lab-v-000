ad4486e65857139671559711e5bf463e2d4ece1c
Catherine Salazar <catcreates13@gmail.com>
2018-01-31 00:59:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\b\w{5}\b/)
+  if text.match(/^[A-Z].+[\.!?]$/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
