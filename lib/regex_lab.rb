23ffdb895fb251b58a427ab54a16b4b891ba006f
Catherine Salazar <catcreates13@gmail.com>
2018-01-31 00:54:52 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if text.scan(/un\w+ing\b/)
+    true
+  else
+    false
+  end
 end
 
 def words_five_letters_long(text)
